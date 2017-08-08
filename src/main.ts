4f7f05893e207d3d48ceed0a96e199a56e27e969
codingInSpace <jonathan.grangien@gmail.com>
2017-08-08 23:01:17 +0200

    Conf ts and implement minimal server

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - Somebody's name
- * @param {number=} [delay=2000] - Number of milliseconds to delay resolution.
- * @returns {Promise<string>}
- */
-function delayedHello(name: string, delay: number = 2000): Promise<string> {
-  return new Promise<string>(
-    (resolve: (value?: string | PromiseLike<string>) => void) => setTimeout(
-      () => resolve(`Hello, ${name}`),
-      delay
-    )
-  );
-}
+import * as express from 'express'
 
-// Below is an example of using TSLint errors suppression
-// Here it's supressing missing type definitions for greeter function
+const app = express()
+const port = process.env.PORT || 1337
 
-export async function greeter(name) { // tslint:disable-line typedef
-  return await delayedHello(name); // tslint:disable-line no-unsafe-any
-}
+app.use('/', (req : express.Request, res : express.Response) => {
+  return res.json({data: 'hello world'})
+})
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
